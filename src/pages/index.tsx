011f3ee2a3b9e0928ba31759eba10ad40bb7e470
Pinot. Kim <pinot.kim@kakao.com>
2020-09-19 08:16:43 +0900

    :sparkles: Add switch templates function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
   NextPage,
 //   NextPageContext,
@@ -10,13 +10,69 @@ import {
 //   getApolloClient,
 // } from "src/hooks";
 
+export type IndexPageStatus = "SPLASH" | "LOGIN" | "HOME";
+
+export type UserAccount = "user1" | "user2" | "admin1" | null
+
 const IndexPage: NextPage = (props) => {
   // return (<div>{props["_"]}</div>);
-  return (
-    <div>
-      Lorem Ipsum...
-    </div>
-  );
+  const [pageStatus, setPageStatus] = useState<IndexPageStatus>('SPLASH');
+  const [userAccount, setUserAccount] = useState<UserAccount>(null);
+  const [isServiceLoaded, setIsServiceLoaded] = useState(false);
+  
+  // 앱의 상태에 따라 useEffect 업데이트
+  useEffect(() => {
+    const [loaded, account, updateStatus] = [isServiceLoaded, userAccount, setPageStatus]
+    
+    // splash 조건 (로그인 X, 서비스 로딩 X)
+    if (!loaded) {
+      updateStatus('SPLASH');
+      return;
+    }
+    
+    // login 조건 (로그인 X, 서비스 로딩 O)
+    if (!account && loaded) {
+      updateStatus('LOGIN');
+      return;
+    }
+    
+    // home 조건 (로그인 O, 서비스 로딩 O)
+    if (account && loaded) {
+      updateStatus('HOME');
+      return;
+    }
+    
+    throw new Error('updatePageStatus function Error');
+  }, [userAccount, isServiceLoaded]);
+  
+  switch (pageStatus) {
+    case "SPLASH":
+      return (
+        <div>
+          splash
+          
+          <button onClick={() => setIsServiceLoaded(true)}>Login</button>
+        </div>
+      );
+    case "LOGIN":
+      return (
+        <div>
+          LOGIN
+          
+          <button onClick={() => setUserAccount('user1')}>USER1</button>
+          <button onClick={() => setUserAccount('user2')}>USER2</button>
+          <button onClick={() => setUserAccount('admin1')}>admin</button>
+        </div>
+      );
+    case "HOME":
+      return (
+        <div>
+          HOME
+        </div>
+      );
+    default :
+      throw new Error(`pageStatus is not correct : ${pageStatus}`);
+  }
 };
 
 // IndexPage.getInitialProps = async (
