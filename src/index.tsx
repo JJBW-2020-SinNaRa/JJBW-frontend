309faf0aece8476bb804be3a64d94e7951008f67
Pinot. Kim <pinot.kim@kakao.com>
2020-09-19 22:05:56 +0900

    :sparkles: upload simple file

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import App from "./App"
 import ReactDOM from 'react-dom';
-import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/react-hooks";
-import {BrowserRouter} from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/react-hooks";
+import { BrowserRouter } from "react-router-dom";
+import { createUploadLink } from "apollo-upload-client";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "/graph",
+  link: createUploadLink({
+    uri: "/graph",
+  })
 })
 
 ReactDOM.render((
   <ApolloProvider client={client}>
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
   </ApolloProvider>
 ), document.getElementById('root'));
--- src/pages/Debug.tsx
+++ src/pages/Debug.tsx
@@ -1,31 +1,49 @@
-import React, {ReactElement, useState} from "react";
-// import {useMutation} from "@apollo/client";
+import React, { ChangeEvent, ReactElement, useState } from "react";
+import { useMutation, gql } from "@apollo/react-hooks";
 
 const UPLOAD = null;
 // const UPLOAD = gql``;
 
+const SINGLE_UPLOAD = gql`
+  mutation UploadSingle($file: Upload!) {
+    uploadSingle(file: $file) {
+      filename
+      mimetype
+      encoding
+    }
+  }
+`;
+
 export const Debug = (): ReactElement => {
   // const [upload] = useMutation(UPLOAD)
   const [file, setFile] = useState(null)
-  
+
   const handleSubmit = (e: any): void => {
     if (!file) {
       return;
     }
-    
+
     console.log(file)
   }
-  
-  const handleFileChange = (e: any): void => {
-    const {name, files} = e.target;
-    setFile(files)
-  
-    // upload
+  const [upload, { data }] = useMutation(SINGLE_UPLOAD);
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const data2 = await upload({
+        variables: {
+          file: file,
+        },
+        
+      });
+      console.log(data, data2);
+    }
   }
-  
+
   return (
     <div>
       <section>
+
         <h3>파일 업로드</h3>
         <input type="file" name="fileTest" onChange={handleFileChange} multiple />
         <button onClick={handleSubmit}>전송하기</button>
