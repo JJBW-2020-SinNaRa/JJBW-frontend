f8b159ece3a894009ae9aa42c72b8acdcc1cce1b
Pinot. Kim <pinot.kim@kakao.com>
2020-09-19 15:17:21 +0900

    :sparkles: Add Account Types

--- src/graphql/login.ts
+++ src/graphql/login.ts
@@ -1,5 +1,14 @@
 import {gql} from "@apollo/client";
 
+export type Account = {
+    idx: number | null;
+    id: string | null;
+    name: string | null;
+    isAdmin: boolean | null;
+    publicKey: string | null;
+    privateKey: string | null;
+}
+
 export const LOGIN_QUERY = gql`
     mutation GetAccessTokenByID($id : String!) {
         getAccessTokenByID(id : $id) {
