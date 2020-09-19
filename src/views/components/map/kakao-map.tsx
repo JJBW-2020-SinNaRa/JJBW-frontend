2b7a84f08d6f20ceda35133b746f847d032e92d1
narumir <openjacoblee@gmail.com>
2020-09-19 23:32:41 +0900

    kakaomap

---
+++ src/views/components/map/kakao-map.tsx
@@ -0,0 +1,19 @@
+import React, {
+  FC,
+  useLayoutEffect,
+  useRef,
+} from "react";
+
+export const KakaoMap: FC = () => {
+  const mapRef = useRef(null);
+  const elementRef = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    const kakao = (window as any).kakao;
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    mapRef.current = new kakao.maps.Map(elementRef.current, options);
+  }, []);
+  return (<div ref={elementRef} style={{ width: "500px", height: "400px" }} />);
+};
