e902f3ef601577fb8f5eb869cff67a5ae06edbe2
Pinot. Kim <pinot.kim@kakao.com>
2020-09-20 14:29:09 +0900

    :sparkles: Add Card

---
+++ src/views/components/Card/Design.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  border-radius: 24px;
+  box-shadow:
+    0 8px 20px 4px rgba(0, 0, 0, 0.1),
+    0 2px 8px 0 rgba(0, 0, 0, 0.2);
+`;
