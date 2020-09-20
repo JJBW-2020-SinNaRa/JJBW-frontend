4a3acac753938e3613480ca6f2efbf73facf9b57
Pinot. Kim <pinot.kim@kakao.com>
2020-09-20 15:31:31 +0900

    :sparkles: Add Some Frm Elem

--- src/views/components/FrmElement/Design.tsx
+++ src/views/components/FrmElement/Design.tsx
@@ -15,6 +15,26 @@ export const Button = styled.button`
     font-weight: ${({current}: BtnProps) => current ? "bold" : "inherit"};
 `
 
+export const Text = styled.p`
+  text-align: center;
+  font-size: 18px;
+  padding: 24px;
+  font-weight: bold;
+  line-height: 1.5;
+`
+
+export const Field = styled.div`
+  h3 {
+    font-size: 20px;
+    margin-bottom: 12px;
+    color: #5C5959;
+  }
+  
+  div:not(:last-child) {
+    margin-bottom: 12px;
+  }
+`
+
 export const Input = styled.input`
     width: 100%;
     height: 100%;
@@ -27,12 +47,13 @@ export const Input = styled.input`
 
 export const Textarea = styled.textarea`
     width: 100%;
-    height: 100%;
     border: none;
     background: none;
-    padding: 6px 24px;
+    padding: 12px 24px;
     font-size: 18px;
     outline: none;
+    resize: none;
+    height: 150px;
 `;
 
 export const SubmitBtn = styled.button`
@@ -41,6 +62,7 @@ export const SubmitBtn = styled.button`
   color: #fff;
   border: none;
   margin: 0 auto;
+  padding: 12px 24px;
   border-radius: 5000px;
   font-size: 18px;
 `
