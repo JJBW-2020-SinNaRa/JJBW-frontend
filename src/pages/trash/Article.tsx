fb55e1d39eb52aeb8c153cb1ea01c235bdff340f
Pinot. Kim <pinot.kim@kakao.com>
2020-09-20 16:03:26 +0900

    :lipstick: Add Style At TrashIndex

--- src/pages/trash/Article.tsx
+++ src/pages/trash/Article.tsx
@@ -1,12 +1,40 @@
 import React, {ReactElement} from "react";
 import {Helmet} from "react-helmet";
 import {Link} from 'react-router-dom';
+import {Card} from "../../views/components/Card/Design";
+import {Field, SubmitBtn} from "../../views/components/FrmElement/Design";
+import styled from "styled-components";
+
+const Container = styled.section`
+  display: grid;
+  grid-row-gap: 36px;
+  
+  h3 {
+    margin-left: 24px;
+  }
+`
+
+const Content = styled.div`
+  padding: 24px;
+  
+  img {
+    display: block;
+    margin: 0 auto;
+    width: 30%;
+  }
+`
+
+const Links = styled.div`
+  a {
+    text-decoration: none;
+  }
+`
 
 export type TrashIndexProps = {
-  id: number
+  id: number;
 }
 
-export const TrashIndexPage = ({ id }: TrashIndexProps): ReactElement => {
+export const TrashIndexPage = ({id}: TrashIndexProps): ReactElement => {
   // TODO : 백엔드 연동
   
   return (
@@ -15,48 +43,64 @@ export const TrashIndexPage = ({ id }: TrashIndexProps): ReactElement => {
         <title>제목</title>
       </Helmet>
       
-      <section>
-        <div>
+      <Container>
+        <Field>
           <h3>보상금</h3>
-          <div>
-            50 KLAY
-          </div>
-        </div>
-        <div>
+          <Card>
+            <Content>
+              50 KLAY
+            </Content>
+          </Card>
+        </Field>
+        <Field>
           <h3>폐기물 설명</h3>
-          <div>
-            Lorem ipsum dolor sit amet.
-          </div>
-        </div>
-        <div>
+          <Card>
+            <Content>
+              Lorem ipsum dolor sit amet.
+            </Content>
+          </Card>
+        </Field>
+        <Field>
           <h3>폐기물 위치</h3>
-          <div>
-            Lorem ipsum dolor sit amet.
-          </div>
-        </div>
-        <div>
+          <Card>
+            <Content>
+              Lorem ipsum dolor sit amet.
+            </Content>
+          </Card>
+        </Field>
+        <Field>
           <h3>폐기물 종류</h3>
-          <div>
-            Lorem ipsum dolor sit amet.
-          </div>
-        </div>
-        <div>
+          <Card>
+            <Content>
+              Lorem ipsum dolor sit amet
+            </Content>
+          </Card>
+        </Field>
+        <Field>
           <h3>발견 시기</h3>
-          <div>
-            YYYY년 MM월 DD일 HH시 MM분
-          </div>
-        </div>
-        <div>
+          <Card>
+            <Content>
+              YYYY년 MM월 DD일 HH시 MM분
+            </Content>
+          </Card>
+        </Field>
+        <Field>
           <h3>업로드 된 사진</h3>
-          <div>
-            <img src="https://placehold.it/300x300" alt=""/>
-          </div>
-        </div>
-        
-        <Link to={`${id}/complete`}>
-          a
-        </Link>
-      </section>
+          <Card>
+            <Content>
+              <img src="https://placehold.it/300x300" alt=""/>
+            </Content>
+          </Card>
+        </Field>
+  
+        <Links>
+          <Link to={`${id}/complete`}>
+            <SubmitBtn>
+              처리하기
+            </SubmitBtn>
+          </Link>
+        </Links>
+      </Container>
     </div>
-  )
+)
 }
