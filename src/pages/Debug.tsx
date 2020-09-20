d42fb4b85315144d573077e2f290a0089f612d9f
Pinot. Kim <pinot.kim@kakao.com>
2020-09-20 13:12:35 +0900

    :sparkles: Add KakaoMap for Debug

--- src/pages/Debug.tsx
+++ src/pages/Debug.tsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent, ReactElement, useState } from "react";
+import React, {ChangeEvent, ReactElement, useEffect, useMemo, useRef, useState} from "react";
 import { useMutation, gql } from "@apollo/react-hooks";
+import {useKakao} from "../views/components/map/kakao-map";
 
 const UPLOAD = null;
 // const UPLOAD = gql``;
@@ -39,6 +40,28 @@ export const Debug = (): ReactElement => {
       console.log(data, data2);
     }
   }
+  
+  const kakao = (window as any).kakao;
+  
+  const mapRef = useRef<any>(null);
+  const elemRef = useRef<HTMLDivElement>(null)
+  
+  const options = useMemo(() => (
+    {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    }
+  ), [])
+  
+  useEffect(() => {
+    const marker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(33.450701, 126.570667)
+    });
+    
+    marker.setMap(mapRef.current)
+  }, [])
+  
+  useKakao(elemRef, mapRef, options)
 
   return (
     <div>
@@ -47,6 +70,12 @@ export const Debug = (): ReactElement => {
         <h3>파일 업로드</h3>
         <input type="file" name="fileTest" onChange={handleFileChange} multiple />
         <button onClick={handleSubmit}>전송하기</button>
+      </section>
+      
+      <section>
+        <h3>카카오맵</h3>
+        
+        <div ref={elemRef} style={{ width: "300px", height: "400px"}} />
       </section>
     </div>
   );
